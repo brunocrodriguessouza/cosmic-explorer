3a38b1d80061dfe38524cc4b1aae0a4aa268c0df
Bruno Cicero Rodrigues de Souza <brunocrodriguessouza@gmail.com>
2023-09-07 15:43:44 -0300

    Cosmic Explorer setup project structure

---
+++ src/main/java/com/brunosouza/cosmic/explorer/CosmicExplorerApplication.java
@@ -0,0 +1,13 @@
+package com.brunosouza.cosmic.explorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CosmicExplorerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CosmicExplorerApplication.class, args);
+	}
+
+}
